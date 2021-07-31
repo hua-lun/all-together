edb507ff9ac9fd53774acc825a1ced8cf44e4b26
hua-lun <chuahualun@gmail.com>
2021-08-01 02:41:40 +0800

    Add twitter bot

---
+++ bot/tweetbot.py
@@ -0,0 +1,24 @@
+import tweepy
+import time
+# NOTE: I put my keys in the keys.py to separate them
+# from this main file.
+# Please refer to keys_format.py to see the format.
+from keys import *
+
+# NOTE: flush=True is just for running this script
+# with PythonAnywhere's always-on task.
+# More info: https://help.pythonanywhere.com/pages/AlwaysOnTasks/
+print('this is my twitter bot', flush=True)
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+
+def reply_to_tweets():
+    print('retrieving and replying to tweets...', flush=True)
+    api.update_status('This is a test')
+    print('success')
+
+
+reply_to_tweets()
