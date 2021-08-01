fdb982f55eb1a51fc135e2d83ba407b7a3e8c37c
hua-lun <chuahualun@gmail.com>
2021-08-01 17:09:01 +0800

    Update twitter bot

--- bot/tweetbot.py
+++ bot/tweetbot.py
@@ -1,3 +1,6 @@
+from datetime import datetime as dt
+from googleapiclient.discovery import build
+import json
 import tweepy
 import time
 # NOTE: I put my keys in the keys.py to separate them
@@ -13,12 +16,82 @@ print('this is my twitter bot', flush=True)
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
+yt = build('youtube', 'v3', developerKey=YT_API_KEY)
+
+counter = 0
+data = data = json.load(open('vidCats.json'))
+arr = ['1', '2', '10', '15', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31',
+       '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44']
+
+print(counter)
 
 
-def reply_to_tweets():
-    print('retrieving and replying to tweets...', flush=True)
-    api.update_status('This is a test')
+def getVid(id):
+    request = yt.videos().list(
+        part="snippet,statistics",
+        chart="mostPopular",
+        regionCode="US",
+        videoCategoryId=str(id),
+        maxResults=1
+    )
+    response = request.execute()
+    return response
+
+
+def tweeting():
+    print('tweets...', flush=True)
+
+    video = getVid(arr[counter])
+    videoInfo = video['items'][0]["snippet"]
+
+    title = videoInfo["title"]
+    time = videoInfo["publishedAt"]
+
+    channel = videoInfo["channelTitle"]
+
+    stats = video['items'][0]["statistics"]["viewCount"]
+
+    output = f"Title: {title}\nViews: {stats}\nChannel: {channel}\nPublished: {time}"
+
+    tt = f"Live from the Raspberry Pi: it is {dt.now()}. \nCategory: {data[arr[counter]]} \n{output}"
+    api.update_status(tt)
     print('success')
 
 
-reply_to_tweets()
+def startingTweet():
+    print("starting")
+
+    cats = []
+
+    request = yt.videos().list(
+        part="snippet",
+        chart="mostPopular",
+        regionCode="US",
+        maxResults=3
+    )
+    response = request.execute()
+    arr = response["items"]
+
+    for i in range(0, len(arr)):
+        catId = arr[i]["snippet"]["categoryId"]
+        cat = data[catId]
+        cats.append(cat)
+
+    c = str(cats)
+    strCats = c.replace("[", "").replace("]", "").replace("'", "")
+    output = f"Bot is live. Today, videos in these categories do well: {strCats}. \nConsider creating videos relating to them!"
+
+    api.update_status(output)
+
+
+try:
+    startingTweet()
+except tweepy.error.TweepError:
+    pass
+
+while True:
+    print(f'here {counter}')
+    tweeting()
+    counter += 1
+    time.sleep(30)
+
